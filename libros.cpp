d2b8fe2fa79933db5d06b5e2d641dc7842f82a40
Cristian Navarrete <cristiannavarrete@outlook.com>
2018-08-07 16:25:21 -0400

    Programa 1 funcionando.

--- libros.cpp
+++ libros.cpp
@@ -9,10 +9,84 @@
 using namespace std;
 
 int main() {
-	int integrantes = 0;
-	int libros = 0;
-	cin >> integrantes;
-	cin >> libros;
+	while(true){
+		int integrantes = 0;
+		int libros = 0;
+		cin >> integrantes;
+		cin >> libros;
+
+		int paginas[libros];
+		int toRead[integrantes];
+		int i = 0;
+		int suma = 0;
+		while(i < libros){
+			cin >> paginas[i];
+			suma+=paginas[i];
+			i++;
+			
+		}
+		int promedio = suma / integrantes;
+
+
+		i = 0;
+		int persona_leyendo = 0;
+		while(i < libros){
+			toRead[persona_leyendo] = 0;
+			while((toRead[persona_leyendo]+paginas[i] <= promedio || persona_leyendo == integrantes-1) && i < libros) {
+				
+				toRead[persona_leyendo]+= paginas[i];
+				i++;
+			}
+
+			persona_leyendo++;
+		}
+
+		
+		
+		i = 0;
+		int maximo = 0;
+		while(i < integrantes){
+			if(toRead[i] > maximo) {
+				maximo = toRead[i];
+			}
+			i++;
+		}
+
+
+		i = libros-1;
+		persona_leyendo = 0;
+		while(i >= 0){
+			toRead[persona_leyendo] = 0;
+			while((toRead[persona_leyendo]+paginas[i] <= promedio || persona_leyendo == integrantes-1) && i >= 0) {
+				
+				toRead[persona_leyendo]+= paginas[i];
+				
+				i--;
+			}
+
+
+			persona_leyendo++;
+		}
+		i = 0;
+		int maximo2 = 0;
+		while(i < integrantes){
+			if(toRead[i] > maximo2) {
+				maximo2 = toRead[i];
+			}
+			i++;
+		}
+		int minimo = 0;
+
+		if(maximo2 > maximo) {
+			minimo = maximo;
+		} else {
+			minimo = maximo2;
+		}
+
+		cout << minimo << endl;
+	}
+
+
 	
-    cout << "Hola que tal" << integrantes << libros << endl;
+
 }
