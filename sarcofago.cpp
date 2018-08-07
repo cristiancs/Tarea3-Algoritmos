05f02fe3e368f52858635515f9a59ef81010c389
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2018-08-07 13:59:17 -0400

    agregado main p2 y gitignore

---
+++ sarcofago.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include <queue>
+#include <sstream>
+#include <string.h>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    cout << "Hola que tal" << endl;
+}
