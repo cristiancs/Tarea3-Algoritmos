cf18e2c3ec042ef41c5fd68feb427567961658bb
Cristian Navarrete <cristiannavarrete@outlook.com>
2018-08-07 14:36:08 -0400

    Archivos bases para libros.

---
+++ libros.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <queue>
+#include <sstream>
+#include <string.h>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+	int integrantes = 0;
+	int libros = 0;
+	cin >> integrantes;
+	cin >> libros;
+	
+    cout << "Hola que tal" << integrantes << libros << endl;
+}
