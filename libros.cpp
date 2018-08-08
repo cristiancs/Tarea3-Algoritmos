10f750fd4386f43d06b37887027203eb713ae5d8
Cristian Navarrete <cristiannavarrete@outlook.com>
2018-08-07 22:45:16 -0400

    En vez de incrementar en 1, incrementamos según las páginas que tiene el libro más pequeño.

--- libros.cpp
+++ libros.cpp
@@ -5,6 +5,7 @@
 #include <set>
 #include <unordered_map>
 #include <unordered_set>
+#include <algorithm>    // std::min
 
 using namespace std;
 
@@ -19,71 +20,48 @@ int main() {
 		int toRead[integrantes];
 		int i = 0;
 		int suma = 0;
+		int paginas_minimas = 9999999;
 		while(i < libros){
 			cin >> paginas[i];
 			suma+=paginas[i];
+			paginas_minimas = min(paginas_minimas, paginas[i]);
 			i++;
 			
 		}
 		int promedio = suma / integrantes;
+		int paginas_promedio = suma / libros;
+		cout << promedio << "|"  << promedio+paginas_promedio << "|" << 89239 << endl;
 
 
-		i = 0;
-		int persona_leyendo = 0;
-		while(i < libros){
-			toRead[persona_leyendo] = 0;
-			while((toRead[persona_leyendo]+paginas[i] <= promedio || persona_leyendo == integrantes-1) && i < libros) {
-				
-				toRead[persona_leyendo]+= paginas[i];
+
+
+		int limiter = paginas_minimas;
+		int min_max = 9999999;
+		while(limiter < suma) {
+			int libro = 0;
+			int persona_leyendo = 0;
+			while(libro < libros){
+				toRead[persona_leyendo] = 0;
+				while((toRead[persona_leyendo]+paginas[libro] <= limiter || persona_leyendo == integrantes-1) && libro < libros) {
+					toRead[persona_leyendo]+= paginas[libro];
+					libro++;
+				}
+				persona_leyendo++;
+			}
+			limiter+=paginas_minimas;
+			i = 0;
+			int maximo = 0;
+			while(i < integrantes){
+				if(toRead[i] > maximo) {
+					maximo = toRead[i];
+				}
 				i++;
 			}
-
-			persona_leyendo++;
+			min_max = min(maximo, min_max);
+			
 		}
-
+		cout << min_max << endl;
 		
-		
-		i = 0;
-		int maximo = 0;
-		while(i < integrantes){
-			if(toRead[i] > maximo) {
-				maximo = toRead[i];
-			}
-			i++;
-		}
-
-
-		i = libros-1;
-		persona_leyendo = 0;
-		while(i >= 0){
-			toRead[persona_leyendo] = 0;
-			while((toRead[persona_leyendo]+paginas[i] <= promedio || persona_leyendo == integrantes-1) && i >= 0) {
-				
-				toRead[persona_leyendo]+= paginas[i];
-				
-				i--;
-			}
-
-
-			persona_leyendo++;
-		}
-		i = 0;
-		int maximo2 = 0;
-		while(i < integrantes){
-			if(toRead[i] > maximo2) {
-				maximo2 = toRead[i];
-			}
-			i++;
-		}
-		int minimo = 0;
-
-		if(maximo2 > maximo) {
-			minimo = maximo;
-		} else {
-			minimo = maximo2;
-		}
-
-		cout << minimo << endl;
 	}
 
 
