2a9b92296953d994599384a22eb7cf5969ae779a
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2018-08-08 18:07:29 -0400

    avances con listas de adyacencia

--- sarcofago.cpp
+++ sarcofago.cpp
@@ -3,9 +3,13 @@
 #include <string.h>
 #include <vector>
 #include <map>
+#include <unordered_map>
+#include <unordered_set>
 
 
 using namespace std;
+
+typedef unordered_map<int, unordered_set<int> > mapa;
 
 struct nodo {
     int n_sarcofago;
@@ -30,18 +34,79 @@ int main() {
         //cout << "Ins " << n_instrucciones << " Dep " << n_dependencias << endl;
         int aux_ins = 0;
         int aux_pes = 0;
+        
+        //Try max size
+        mapa ady;
+        mapa dep;
+        unordered_set<int> disponibles;
+        // Leer los pesos de cada sarcofago
         for (int i=0; i<n_instrucciones; i++) {
             cin >> aux_ins >> aux_pes;
             pesos[aux_ins] = aux_pes;
+            disponibles.insert(aux_ins);
             //cout << "Ins: " << aux_ins << " Pes: "<<aux_pes<<endl;
+            
         }
 
         int i1=0;
         int i2=0;
+        
+        // Leer las dependencias de cada sarcofago
         for (int i =0; i<n_dependencias; i++) {
             cin >> i1 >> i2;
             //cout << "i1: "<<i1<<" i2: "<<i2<<endl;
+            /*
+            if(ady.find(i1) == ady.end()){
+                ady[i1].insert(i2);
+            }
+            else {
+                ady[i1].insert(i2);
+            }
+
+            if(dep.find(i2) == ady.end()){
+                unordered_set<int> dependiente = { i1 };
+                dep[i2] = dependiente);
+            }
+            else {
+                dep[i2].insert(i1);
+            }*/
+            // Actualizar disponibles
+            disponibles.erase(i2);
+            // Meter en listas de adyacencia
+            ady[i1].insert(i2);
+            dep[i2].insert(i1);
         }
+        unordered_set<int> vistos;
+        while (!ady.empty()) {
+            int max = 0;
+            int actual = 0;
+
+            // Elergir el de mayor duracion disponible
+            for ( auto it = disponibles.begin(); it != disponibles.end(); ++it ) {
+                if (pesos[*it] > max) {
+                    actual = *it;
+                    max = pesos[*it];
+                }
+            }
+
+            disponibles.erase(actual);
+            if (!ady[actual].empty()) {
+                // Eliminar dependencias del actual
+                for ( auto it = ady[actual].begin(); it != ady[actual].end(); ++it ) {
+                    dep[*it].erase(actual);
+                    if (dep[*it].empty()) {
+                        disponibles.insert(*it);
+                    }
+                }
+            }
+            // Eliminar de los que faltan por recorrer
+            ady.erase(actual);
+            // Imprimir
+            cout << actual << " ";
+        }
+        cout << endl;
+
+
        // cout<<endl;
     }
         
