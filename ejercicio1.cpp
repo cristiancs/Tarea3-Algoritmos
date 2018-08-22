dd9ffdca97b5bbffc2321fc2be8153a80a747b62
Cristian Navarrete <cristiannavarrete@outlook.com>
2018-08-22 18:44:04 -0300

    agregar comentarios.

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -10,12 +10,19 @@ bool dieta(int maxCal, int alimentos[], int n) {
 	flag = false;
 	for (i = 0; i <= n; i++) {
 		for (calorias = 0; calorias <= maxCal; calorias++) {
-			if( i==0 || calorias==0 )
+
+			// Offset en la tabla
+			if( i==0 || calorias==0 ){
 				aux[i][calorias] = 0;
-			else if ( alimentos[i-1] <= calorias )
+			}
+			else if ( alimentos[i-1] <= calorias ) {
+				// Función recursiva
 				aux[i][calorias] = max(alimentos[i-1] + aux[i-1][calorias-alimentos[i-1]],  aux[i-1][calorias]);
-			else
+			}
+			else {
 				aux[i][calorias] = aux[i-1][calorias];
+			}
+			// Si encontramos uno que cumple la condición terminamos el ciclo
 			if ( aux[i][calorias] == maxCal ){
 				flag = true;
 				break;
