41d18ed5af93b62922e034569e5379347dff8057
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2018-08-26 04:01:02 -0300

    avance llenado manual

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,118 +1,122 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
+#include <stack>
+#include <vector>
 
 using namespace std;
  
-// dp[i][j] is going to store true if sum j is
-// possible with array elements from 0 to i.
-bool** dp;
+
 typedef unordered_map<int, int> mapa;
 
-void getCodigo(mapa& cod_peso, const vector<int>& v) {
-	
-	//cout << "codigos: "<<endl;
-    for (int i = 0; i < v.size(); ++i) {
-		mapa::iterator it = cod_peso.find(v[i]);
-		//auto it = cod_peso.find(v[i]);
-		//int num = it.second;
-		cout << endl;
-		for (auto it = cod_peso.begin(); it != cod_peso.end(); ++it) {
-			cout << "peso: " << it->first << " || cod: " << it->second<<endl;
+void allSubsetSum(vector<int> pesos, int n_alimentos, int calorias)
+{
+    // The value of subset[i][j] will be true if there is a 
+    // subset of set[0..j-1] with sum equal to i
+    bool dp[n_alimentos][calorias+1];
+  
+    // If sum is 0, then answer is true
+    for (int i = 0; i <= n_alimentos; i++) dp[i][0] = true;
+  
+    // If sum is not 0 and set is empty, then answer is false
+    //for (int i = 1; i <= calorias; i++) dp[0][i] = false;
+  
+     // Fill the dp table in botton up manner
+     for (int i = 0; i < n_alimentos; i++) {
+       for (int j = 1; j <= calorias; j++) {
+		   	if (i == 0) {
+				if (j - pesos[i] == 0) {
+					dp[i][j] = true;
+				}
+				else {
+					dp[i][j] = false;
+
+				}
+		   	}
+			else {
+				if (j<pesos[i]) {
+					dp[i][j] = dp[i-1][j];
+				}
+				else {
+        			dp[i][j] = dp[i-1][j] || dp[i - 1][j-pesos[i]];
+				}
+			}
+       }
+     }
+  
+        // uncomment this code to print table
+     for (int i = 0; i < n_alimentos; i++){
+       	for (int j = 0; j <= calorias; j++) {
+        	printf ("%4d", dp[i][j]);
+	   	}
+       printf("\n");
+     }
+  
+     //return dp;
+
+	 // Buscar posibles soluciones
+
+	 // No era posible
+	 if (dp[n_alimentos-1][calorias] == 0){
+		 cout << "0" << endl;
+	 }
+
+	// Es posible
+	else {
+		stack <int> incluidos;
+		int sum_actual = calorias;
+		int i = n_alimentos-1;
+		while (i >= 0) {
+			if (dp[i][sum_actual] == 1) {
+				incluidos.push(pesos[i]);
+				//i = sum_actual-pesos[i];
+				i = distance(pesos.begin(), find(pesos.begin(), pesos.end(), pesos[i]));
+
+			}
+			else {
+				i--;
+			}
+			if (i==0) {
+				// Se llegó al final de la matriz
+
+				// Copiar
+
+
+				// Seguir con otras soluciones
+				if (incluidos.size() == 0) break;
+				int peso_actual = incluidos.top();
+				sum_actual += peso_actual;
+				incluidos.pop();
+				i = distance(pesos.begin(), find(pesos.begin(), pesos.end(), peso_actual))-1;
+				if (i<0) break;
+				continue;
+			}
+			if (sum_actual == 0) {
+				// Se llegó a una solución
+
+				// Copiar
+
+				// Seguir
+				int peso_actual = incluidos.top();
+				sum_actual += peso_actual;
+				incluidos.pop();
+				i = distance(pesos.begin(), find(pesos.begin(), pesos.end(), peso_actual))-1;
+
+			}
 		}
-        //printf("%d ", it->second);
-		printf("%d ", v[i]);
 	}
-	cout << endl;
-	/*cout << "pesos: "<<endl;
-	for (int i = 0; i < v.size(); ++i) {
-        printf("%d ", v[i]);
-	}
-	cout <<endl;
-    printf("\n");*/
 }
- 
-// A recursive function to print all subsets with the
-// help of dp[][]. Vector p[] stores current subset.
-void printSubsetsRec(mapa& cod_peso, int arr[], int i, int sum, vector<int>& p) {
-    // If we reached end and sum is non-zero. We print
-    // p[] only if arr[0] is equal to sun OR dp[0][sum]
-    // is true.
-    if (i == 0 && sum != 0 && dp[0][sum])
-    {
-        p.push_back(arr[i]);
-        getCodigo(cod_peso, p);
-        return;
-    }
- 
-    // If sum becomes 0
-    if (i == 0 && sum == 0)
-    {
-        getCodigo(cod_peso, p);
-        return;
-    }
- 
-    // If given sum can be achieved after ignoring
-    // current element.
-    if (dp[i-1][sum])
-    {
-        // Create a new vector to store path
-        vector<int> b = p;
-        printSubsetsRec(cod_peso, arr, i-1, sum, b);
-    }
- 
-    // If given sum can be achieved after considering
-    // current element.
-    if (sum >= arr[i] && dp[i-1][sum-arr[i]])
-    {
-        p.push_back(arr[i]);
-        printSubsetsRec(cod_peso, arr, i-1, sum-arr[i], p);
-    }
-}
- 
-// Prints all subsets of arr[0..n-1] with sum 0.
-void printAllSubsets(mapa& cod_peso, int arr[], int n, int sum) {
-    if (n == 0 || sum < 0)
-       return;
- 
-    // Sum 0 can always be achieved with 0 elements
-    dp = new bool*[n];
-    for (int i=0; i<n; ++i) {
-        dp[i] = new bool[sum + 1];
-        dp[i][0] = true;
-    }
- 
-    // Sum arr[0] can be achieved with single element
-    if (arr[0] <= sum)
-       dp[0][arr[0]] = true;
- 
-    // Fill rest of the entries in dp[][]
-    for (int i = 1; i < n; ++i)
-        for (int j = 0; j < sum + 1; ++j)
-            dp[i][j] = (arr[i] <= j) ? dp[i-1][j] ||
-                                       dp[i-1][j-arr[i]]
-                                     : dp[i - 1][j];
-    if (dp[n-1][sum] == false)
-    {
-        printf("0\n");
-        return;
-    }
- 
-    // Now recursively traverse dp[][] to find all
-    // paths from dp[n-1][sum]
-    vector<int> p;
-    printSubsetsRec(cod_peso, arr, n-1, sum, p);
-}
- 
+
 // Driver code
 int main()
 {	
 	int n_alimentos, k_cal_esperadas, id_alim, w_alim, i;
 	while(cin >> n_alimentos >> k_cal_esperadas){
 		mapa cod_peso;
-		int array_w[n_alimentos];
+		vector<int> array_w(n_alimentos);
 
-		for (i = 0; i < n_alimentos; ++i) {
+		for (i = 0; i < n_alimentos; i++) {
 			cin >> id_alim >> w_alim;
 			array_w[i] = w_alim;
 			int w = w_alim;
@@ -121,11 +125,12 @@ int main()
 			cod_peso.insert(pair<int, int>(w, id));
 		}
 
-		/*cout << "n_alimentos: " << n_alimentos << "  calorias a sum: "<< k_cal_esperadas<<endl;
-		for (auto it = cod_peso.begin(); it != cod_peso.end(); ++it) {
+		cout << "n_alimentos: " << n_alimentos << "  calorias a sum: "<< k_cal_esperadas<<endl;
+		/*for (auto it = cod_peso.begin(); it != cod_peso.end(); ++it) {
 			cout << "peso: " << it->first << " || cod: " << it->second<<endl;
 		}*/
-		printAllSubsets(cod_peso, array_w, n_alimentos, k_cal_esperadas);
+		sort(array_w.begin(), array_w.end());
+		allSubsetSum(array_w, n_alimentos, k_cal_esperadas);
 		cout << endl;
 	}
     return 0;
